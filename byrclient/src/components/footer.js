dfe728c707b3151754d0b971357d4133b86439ae
Corbin Taylor <cptaylor38@yahoo.com>
2020-06-10 23:29:42 -0400

    removed map feature

--- byrclient/src/components/footer.js
+++ byrclient/src/components/footer.js
@@ -1,48 +1,11 @@
 import React from "react"
-import Map from "../components/map"
 
 const Footer = () => {
   return (
     <footer>
-      <div id="links">
-        <div>link 1</div>
-        <div>link 2</div>
-        <div>link 3</div>
-      </div>
-      <div id="mapContainer">
-        <Map
-          isMarkerShown
-          googleMapURL={process.env.GATSBY_gmURL}
-          loadingElement={
-            <div
-              style={{
-                height: `100%`,
-                padding: "1rem",
-                marginRight: "1em",
-              }}
-            />
-          }
-          containerElement={
-            <div
-              style={{
-                height: `200px`,
-                padding: "1rem",
-                marginRight: "1em",
-              }}
-            />
-          }
-          mapElement={
-            <div
-              style={{
-                height: `80%`,
-                padding: "1rem",
-                marginRight: "1em",
-              }}
-            />
-          }
-          id="mapComponent"
-        />
-      </div>
+      <div>link 1</div>
+      <div>link 2</div>
+      <div>link 3</div>
     </footer>
   )
 }
