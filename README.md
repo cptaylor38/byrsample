99d18c0aac9f988bafe186a566edbe7531ce07c7
Corbin Taylor <cptaylor38@yahoo.com>
2020-06-16 01:40:17 -0400

    last up contact page

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Remaking a local business' website using Gatsby.
 [Original](http://thebackyardretreat.com/)
 
 [Work in progress](https://nervous-khorana-ffc875.netlify.app/)
+
+Attributions:
+
+[Background photo courtsey of Martin Kníže on Unsplash.](https://unsplash.com/photos/JjT_7MwREm4)
+
+[Vines SVG background courtsey of Tyler Porter on clker.com](http://www.clker.com/clipart-vine-border-27.html)
--- byrclient/src/pages/tips.js
+++ byrclient/src/pages/tips.js
@@ -5,7 +5,108 @@ const TipsPage = () => {
   return (
     <Layout>
       <main>
-        <h1>Tips</h1>
+        <section class="tips-section">
+          <p class="tips-header">
+            Tips from Tom on{" "}
+            <a
+              href="https://www.youtube.com/channel/UCgR4YjSUOh3H9zf5uRRKGUg"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Hometown TV Today
+            </a>
+          </p>
+          <div class="seasonal-tips">
+            <p class="tips-header">Seasonal Tips</p>
+            <div class="tips-iframe-container">
+              <h2 class="tips-subheader">Summer Chores</h2>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/md1mAXsuOpI"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                allowFullscreen
+                title="Summer Chores"
+              ></iframe>
+            </div>
+            <div class="tips-iframe-container">
+              <h2 class="tips-subheader">Summer Watering</h2>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/Ww5Gid-B38Q"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                allowFullscreen
+                title="Summer Watering"
+              ></iframe>
+            </div>
+            <div class="tips-iframe-container">
+              <h2 class="tips-subheader">Fall Planting</h2>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/9NzXRDJQDB4"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                allowFullscreen
+                title="Fall Planting"
+              ></iframe>
+            </div>
+            <div class="tips-iframe-container">
+              <h2 class="tips-subheader">Winter Gardening</h2>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/RWHj3amO5cQ"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                allowFullscreen
+                title="Winter Gardening"
+              ></iframe>
+            </div>
+          </div>
+          <div class="msc-tips">
+            <p class="tips-header">More Tips!</p>
+            <div class="tips-iframe-container">
+              <h2 class="tips-subheader">Container Gardening</h2>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/KB1ZujuNvHs"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                allowFullscreen
+                title="Planting Tips"
+              ></iframe>
+            </div>
+            <div class="tips-iframe-container">
+              <h2 class="tips-subheader">Raised Bed Gardens</h2>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/N0oPkASCyDw"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                allowFullscreen
+                title="Raised Bed Gardens"
+              ></iframe>
+            </div>
+            <div class="tips-iframe-container">
+              <h2 class="tips-subheader">Planting Bulbs</h2>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/x0r28Dbc7I0"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                allowFullscreen
+                title="Planting Bulbs"
+              ></iframe>
+            </div>
+          </div>
+        </section>
       </main>
     </Layout>
   )
